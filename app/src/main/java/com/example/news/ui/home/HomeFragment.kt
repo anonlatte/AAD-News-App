66a4f3458aa67b133bdc40bb927369577023bc27
anonlatte <biba.slow15@gmail.com>
2020-06-08 21:17:21 +0500

    refactor remove useless template code

--- app/src/main/java/com/example/news/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/news/ui/home/HomeFragment.kt
@@ -4,28 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.example.news.R
+import androidx.fragment.app.viewModels
+import com.example.news.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        return binding.root
     }
 }
